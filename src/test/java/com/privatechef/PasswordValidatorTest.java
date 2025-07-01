fd989796c87069b20457f3d531b14a856f05c66a
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-01 11:46:48 +0200

    add failing tests

---
+++ src/test/java/com/privatechef/PasswordValidatorTest.java
@@ -0,0 +1,78 @@
+package com.privatechef;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidatorTest {
+    PasswordValidator passwordValidator;
+
+    @BeforeEach
+    void setUp(){
+        passwordValidator =   new PasswordValidator("");
+    }
+
+    @Test
+    void validate() {
+        passwordValidator.setPassword("!12WisheasdDALlkwq");
+        String expect =  "Valid Password";
+
+        assertEquals(expect,passwordValidator.validate());
+    }
+
+    @Test
+    void checkLength() {
+        passwordValidator.setPassword("Short");
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () ->
+                passwordValidator.checkLength()
+        );
+        assertEquals("Password length should be between 8 and 16 characters", ex.getMessage());
+    }
+
+    @Test
+    void checkNumber() {
+        passwordValidator.setPassword("NoNumber");
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () ->
+                passwordValidator.checkNumber()
+        );
+        assertEquals("Password length should be between 8 and 16 characters", ex.getMessage());
+    }
+
+    @Test
+    void checkUppercaseLetters() {
+        passwordValidator.setPassword("uppercaseletters");
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () ->
+                passwordValidator.checkUppercaseLetters()
+        );
+        assertEquals("Password length should be between 8 and 16 characters", ex.getMessage());
+    }
+
+    @Test
+    void checkLowercaseLetters() {
+        passwordValidator.setPassword("LOWERCASELETTERS");
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () ->
+                passwordValidator.checkLowercaseLetters()
+        );
+        assertEquals("Password length should be between 8 and 16 characters", ex.getMessage());
+    }
+
+    @Test
+    void checkWeekPassword() {
+        passwordValidator.setPassword("password");
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () ->
+                passwordValidator.checkWeekPassword()
+        );
+        assertEquals("Password length should be between 8 and 16 characters", ex.getMessage());
+    }
+
+    @Test
+    void checkSpecialCharacters() {
+        passwordValidator.setPassword("Password01");
+
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () ->
+                passwordValidator.checkSpecialCharacters()
+        );
+        assertEquals("Password length should be between 8 and 16 characters", ex.getMessage());
+    }
+}
