2d0eba2c6252319224be9b50513da45bb0b36c47
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-01 13:01:59 +0200

    create ExceptionMessage class

---
+++ src/main/java/com/privatechef/ExceptionMessages.java
@@ -0,0 +1,12 @@
+package com.privatechef;
+
+public class ExceptionMessages {
+    public String VALID_PASSWORD = "Valid Password";
+    public String CHECK_LENGTH = "Password should be between 8 and 16 characters";
+    public String CHECK_NUMBER = "Password should have at least 1 number";
+    public String CHECK_UPPERCASE = "Password should have at least 1 uppercase letter";
+    public String CHECK_LOWERCASE = "Password should have at least 1 lowercase letter";
+    public String CHECK_IS_TO_WEEK = "Password is too weak";
+    public String CHECK_HAS_SPECIAL_CHAR = "Password should have at least 1 special character";
+
+}
