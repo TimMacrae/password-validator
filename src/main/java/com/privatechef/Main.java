1239273a7d93e7ba3a2761e9f8ebac298ca1faf1
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-02 09:26:58 +0200

    refactor main method to ask for generate or use own password

--- src/main/java/com/privatechef/Main.java
+++ src/main/java/com/privatechef/Main.java
@@ -3,11 +3,27 @@ package com.privatechef;
 import java.util.Scanner;
 
 public class Main {
+    static final ExceptionMessages exceptionMessages = new ExceptionMessages();
     public static void main(String[] args) {
-        ExceptionMessages exceptionMessages = new ExceptionMessages();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type GENERATE to generate a password or OWN to type your own password");
+
+        String decision = scanner.nextLine();
+        if (decision.equals("OWN")) {
+            validateUserPassword();
+        }
+        if(decision.equals("GENERATE")) {
+            generateUserPassword();
+        }
 
 
-        String password = getPasswordInput();
+    }
+
+    public static void validateUserPassword(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your password:");
+
+        String password = scanner.nextLine();
         PasswordValidator passwordValidator = new PasswordValidator(password);
 
         try {
@@ -18,9 +34,9 @@ public class Main {
         }
     }
 
-    public static String getPasswordInput(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your password:");
-        return scanner.nextLine();
+    public static void generateUserPassword(){
+        PasswordValidator passwordValidator = new PasswordValidator("");
+        String generatedPassword = passwordValidator.generatePassword();
+        System.out.println("You password is: " + generatedPassword);
     }
 }
